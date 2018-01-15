742df48901df6dd007fa81c6c620aeafe1f5636e
Chandan CH <chandanch@users.noreply.github.com>
2018-01-15 15:12:14 +0530

    function with return types

--- functions.swift
+++ functions.swift
@@ -23,3 +23,33 @@ func showPlayerDetails(name: String, score: Int, email: String) {
 // call a function by specifying the argument label and its value delimited by (:)
 // syntax: <function_name>(argumentLabel:<value>,...)
 showPlayerDetails(name: "Heroku", score: 340, email: "heroku@swift.com")
+
+// function with parameters and which returns value
+func calculateTotalScore(level1Score: Int, level2Score: Int) -> Int {
+	let totalScore = level1Score + level2Score
+	return totalScore
+}
+
+var playerScore = calculateTotalScore(level1Score: 300, level2Score: 300)
+print("Player's Total Score is \(playerScore)");
+
+// Function type: We define a function type by looking at the function 
+// parameters and its return type
+// The below function type is: This function takes a single parameter of int type and returns a string
+func assignBadgeToPlayer(playerScore: Int) -> String {
+	switch playerScore {
+		case 0...200:
+			return "Amature"
+		case 201...400:
+		return "Semi-pro"
+		case 401...600:
+			return "pro"
+		case 601...1000:
+			return "Legend"
+		default:
+			return "Invalid"
+	}
+}
+
+let playerBadge = assignBadgeToPlayer(playerScore: playerScore)
+print("Player badge is \(playerBadge)")
