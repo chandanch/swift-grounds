028db2fcdca526f2349029625fe9dcb6a063f6fb
Chandan CH <chandanch@users.noreply.github.com>
2018-02-04 11:13:59 +0530

    update desc stride

--- stride.swift
+++ stride.swift
@@ -4,12 +4,12 @@
 * There are two types of stride: 1.Stride through 2. Stride to
 */
 
-// strideThrough: stride through from 0 to 200 including 0 and 200(closed range)
+// strideThrough: stride through from 0 to 200 in steps of 10 including 0 and 200(closed range)
 for number in stride(from: 0, through: 200, by: 10) {
   // print(number)
 }
 
-// StrideTo: stride from 0 to till 256 in steps of 24 not including 256(hlaf closed range)
+// StrideTo: stride from 0 to till 256 in steps of 24 not including 256(half closed range)
 for number in stride(from: 0, to: 256, by: 24) {
   print(number)
 }
