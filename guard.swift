ce62c7d8e69739f377f1b8f383b766046a0a59e9
Chandan CH <chandanch@users.noreply.github.com>
2018-02-16 13:07:51 +0530

    combing multiple guards

--- guard.swift
+++ guard.swift
@@ -14,3 +14,13 @@ func showPlayerDetails(name: String?, score: Int?, flower: String?) {
 }
 
 showPlayerDetails(name: "Fowler", score: 23, flower: "Gop")
+
+// combining multiple guard statments using , and using a single else
+func showGameDetails(name: String?, score: Int?, flower: String?) {
+	// check if the optional paramter is nil. If not nil save the value in a vsariable or constant. If nil exist immediately
+	guard let playerName = name,
+	guard let playerScore = score,
+	guard let playerFlower = flower else {return}
+	// using the variable holding the optional value once the guard statement is over
+	print("Player name: \(playerName)\nPlayer Score: \(playerScore)\nPlayer Flower: \(playerFlower)")
+}
